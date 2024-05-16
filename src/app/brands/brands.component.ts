d951a2e06ebc9ee26e17c61c4fe1b7f14b71c651
Plamen M <pulsedloops@gmail.com>
2024-05-16 10:34:46 +0200

    [refactor]: Clearing & Organising Sass & Html classes part 01

--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {Brand} from "../models/brand";
 import {Product} from "../models/product";
-import {AngularFireDatabase} from "@angular/fire/compat/database";
 import {ProductService} from "../services/product.service";
 import {BrandService} from "../services/brand.service";
 
@@ -16,8 +15,7 @@ export class BrandsComponent implements OnInit{
   brands$!: Observable<Brand[]>
   products$!: Observable<Product[]>
 
-  constructor(private db: AngularFireDatabase,
-              private productService: ProductService,
+  constructor(private productService: ProductService,
               private brandsService: BrandService) { }
 
   ngOnInit(): void {
