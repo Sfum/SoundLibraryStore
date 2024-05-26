0971efdabd6eb2b83ef8a317c023c67818a9a54e
Plamen M <pulsedloops@gmail.com>
2024-05-26 21:24:57 +0200

    [fix]: Add To Cart & Wishlist Button fixes in On Sale component

--- src/app/on-sale/on-sale.component.ts
+++ src/app/on-sale/on-sale.component.ts
@@ -1,8 +1,25 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { BrandService } from '../services/brand.service';
+import { GenreService } from '../services/genre.service';
+import { WishlistService } from '../services/wishlist.service';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-on-sale',
   templateUrl: './on-sale.component.html',
   styleUrl: './on-sale.component.sass',
 })
-export class OnSaleComponent {}
+export class OnSaleComponent {
+  constructor(
+    private wishlistService: WishlistService,
+    private cartService: CartService,
+  ) {}
+  onAddToWishlist(product: any) {
+    this.wishlistService.addToWishlist(product);
+  }
+
+  onAddToCart(product: any) {
+    this.cartService.addToCart(product);
+  }
+}
